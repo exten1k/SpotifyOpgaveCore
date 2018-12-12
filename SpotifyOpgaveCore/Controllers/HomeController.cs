ddb6bc16b08ce033718d6acbd4905c7aa32730ac
jurij <jurij@LAPTOP-32SOJENL>
2018-12-12 16:51:20 +0100

    Add project files.

---
+++ SpotifyOpgaveCore/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Spotify.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("~/")]
+        public async System.Threading.Tasks.Task<ActionResult> Index()
+        {
+            ViewBag.UserName = await HttpContext.GetTokenAsync("Spotify", "refresh_token");
+            return View();
+        }
+        [Authorize]
+        
+        public async System.Threading.Tasks.Task<ActionResult> Test()
+        {
+            ViewBag.Token = await HttpContext.GetTokenAsync("Spotify", "access_token");
+            return View("Test");
+        }
+    }
+}
---
+++ SpotifyOpgaveCore/Startup.cs
@@ -0,0 +1,82 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+
+namespace SpotifyOpgaveCore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+            })
+
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/signin";
+                options.LogoutPath = "/signout";
+                options.ExpireTimeSpan = TimeSpan.FromHours(24);
+            })
+
+            .AddSpotify(options =>
+            {
+                options.ClientId = "49d56ad20adc4096a2e082401e817dcf";
+                options.ClientSecret = "f11e461115a043769bfea0fc56384aee";
+                options.SaveTokens = true;
+                options.Scope.Add("playlist-modify-private");
+                options.UserInformationEndpoint = "https://api.spotify.com/v1/me";
+
+                options.Events = new OAuthEvents
+                {
+                    OnCreatingTicket = async context =>
+                    {
+                        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                        HttpResponseMessage response = await context.Backchannel.SendAsync(request, context.HttpContext.RequestAborted);
+                        response.EnsureSuccessStatusCode();
+
+                        var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                        var name = user.Value<string>("display_name");
+                        if (!string.IsNullOrEmpty(name))
+                        {
+                            context.Identity.AddClaim(new Claim(ClaimTypes.Name, name, ClaimValueTypes.String, context.Options.ClaimsIssuer));
+                        }
+                    }
+                };
+            });
+
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
