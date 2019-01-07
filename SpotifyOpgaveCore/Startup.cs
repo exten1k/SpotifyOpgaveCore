6bf955b61cdffb6652c3ca000bc64b33c1d53500
Jurijs Mahtodujs <exten1k@gmail.com>
2019-01-07 15:31:38 +0100

    refresh token event

--- SpotifyOpgaveCore/Startup.cs
+++ SpotifyOpgaveCore/Startup.cs
@@ -4,18 +4,24 @@
  * for more information concerning the license and the contributors participating to this project.
  */
 
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Linq;
 using SpotifyOpgaveCore.Models;
 using System;
+using System.Globalization;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace SpotifyOpgaveCore
 {
@@ -28,13 +34,46 @@ namespace SpotifyOpgaveCore
             services.AddAuthentication(options =>
             {
                 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-
             })
 
             .AddCookie(options =>
             {
                 options.LoginPath = "/signin";
                 options.LogoutPath = "/signout";
+                options.Events = new CookieAuthenticationEvents
+                {
+                    OnValidatePrincipal = context =>
+                    {
+                        if (context.Principal.Identity.IsAuthenticated)
+                        {
+                            var tokens = context.Properties.GetTokens();
+                            var refreshToken = tokens.FirstOrDefault(t => t.Name == "refresh_token");
+                            var accessToken = tokens.FirstOrDefault(t => t.Name == "access_token");
+                            var exp = tokens.FirstOrDefault(t => t.Name == "expires_at");
+                            var expires = DateTime.Parse(exp.Value);
+                            if (expires < DateTime.Now)
+                            {
+                                var tokenEndpoint = "https://accounts.spotify.com/api/token";
+                                string clientId = "49d56ad20adc4096a2e082401e817dcf";
+                                string clientSecret = "f11e461115a043769bfea0fc56384aee";
+                                var tokenClient = new TokenClient(tokenEndpoint, clientId, clientSecret);
+                                var tokenResponse = tokenClient.RequestRefreshTokenAsync(refreshToken.Value).Result;
+                                if (tokenResponse.IsError)
+                                {
+                                    context.RejectPrincipal();
+                                    return Task.CompletedTask;
+                                }
+                                accessToken.Value = tokenResponse.AccessToken;
+                                var newExpires = DateTime.UtcNow + TimeSpan.FromSeconds(tokenResponse.ExpiresIn);
+                                exp.Value = newExpires.ToString("o", CultureInfo.InvariantCulture);
+                                context.Properties.StoreTokens(tokens);
+                                context.ShouldRenew = true;
+                                return Task.CompletedTask;
+                            }
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
             })
 
             .AddSpotify(options =>
